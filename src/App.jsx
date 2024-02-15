72cd09c38907ba95abcd2ebdedecf52f00d97bfb
Ryan Hemlock <ryan@ryanhemlock.com>
2024-02-15 10:11:38 -0500

    Updated URI

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import NavBar from './components/NavBar.jsx';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: 'https://teamsync-server-kedc.onrender.com/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
